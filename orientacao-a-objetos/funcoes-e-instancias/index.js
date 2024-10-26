0ff76df480afefdcb4037943e80c0d87aac6ff38
alexandretoscano23 <toscanodinizalexandre32@gmail.com>
2024-10-25 21:44:05 -0300

    Update index.js

--- orientacao-a-objetos/funcoes-e-instancias/index.js
+++ orientacao-a-objetos/funcoes-e-instancias/index.js
@@ -7,4 +7,14 @@ function UserName(){
 
 let userName = new UserName(); //criando um novo objeto chamado userName
 
-alert(userName.name); //mostra um alerta na tela com o valor de name
+//alert(userName.name); //mostra um alerta na tela com o valor de name
+
+//funções
+
+window.onload = function(){ //executar os códigos quando a página estiver 100% carregada...
+
+
+
+
+
+};
