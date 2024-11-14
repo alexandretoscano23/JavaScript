77d0fee35a51c971a4ccbb2017170f323d78d996
alexandretoscano23 <toscanodinizalexandre32@gmail.com>
2024-11-14 17:21:33 -0300

    css pelo jquery
    Estilizando elementos pelo jQuery

--- jQuery/manipulando-css/script.js
+++ jQuery/manipulando-css/script.js
@@ -1,4 +1,20 @@
 $(window).on('load', function(){
-    
+    //mudar a cor de fundo da div
+    $('div').css('background-color', '#ccc').css('color', 'darkred');
 
+    // padding - tem que definir cada lado
+    $('div').css('padding-left', '20px');
+    $('div').css('padding-right', '20px');
+    $('div').css('padding-top', '20px');
+    $('div').css('padding-bottom', '20px');
+
+    // chamando por id
+    setTimeout(function(){
+        $('#test').css('background-color', 'lightblue');
+    }, 2000);
+
+    // pegar um elemento dentro de outro
+    setTimeout(()=>{
+        $('div > p').css('color','green');
+    }, 2000);
 });
