cbba92f7936a98a9a33d70a5f42f232ebfc30dbe
alexandretoscano23 <162184671+alexandretoscano23@users.noreply.github.com>
2024-12-13 22:54:56 -0300

    Update reduce.js

--- src/objetos/array/loop/reduce/reduce.js
+++ src/objetos/array/loop/reduce/reduce.js
@@ -13,3 +13,22 @@ const result = students.map(a => a.score).reduce(function(acumulator, actual){
 
 console.log(result);
 
+//somar todos os preços dos produtos
+const products = [
+  { name: "TV", price: 900 },
+  { name: "Notebook", price: 1200 },
+  { name: "Iphone", price: 1500 },
+  { name: "Samsung", price: 1000 },
+  { name: "Xiaomi", price: 700 },
+  { name: "Tablet", price: 400 },
+  { name: "Smartwatch", price: 300 },
+  { name: "Headphone", price: 100 },
+];
+
+//selecionar apenas o valor e somar o preço total
+const result2 = products.map(p => p.price).reduce(function(acumulador,actual){
+  console.log(acumulador, actual)
+  return acumulador + actual
+});
+
+console.log(result2);
